57c8bdeabf6ce19667f0208026e07778c82dffa8
Dakota Newton <dakotajn@outlook.com>
2020-05-02 14:06:08 -0700

    Lots of stuff. not sure where the 900+ files came from... oh well

---
+++ Dak Python Practice.py
@@ -0,0 +1,30 @@
+PauseVideo = "38:20"
+
+CharName = "Dakota"
+CharAge = 26
+isMale = True
+
+print()
+
+print("my name is " + CharName)
+
+print("Create a line \nmid string")
+
+print(CharName.lower() + CharName.upper())
+
+print(CharName.upper().isupper())
+
+print(CharName[0])
+
+print(CharName.index("ako"))
+
+if True:
+    print(CharAge * 2)
+
+stocks = ["SQ", "AMZN", "FB"]
+
+print(stocks[0])
+
+i = 0
+
+
